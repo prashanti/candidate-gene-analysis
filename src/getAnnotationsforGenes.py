b47e5bbb883633759d42cfd20542c25199c07b6b
prashanti <manda.prashanti@gmail.com>
2014-12-02 13:24:03 -0500

    Added script to pull EQ annotations for genes from the Phenoscape KB

---
+++ src/getAnnotationsforGenes.py
@@ -0,0 +1,42 @@
+def main():
+	genes=open('zfin-genes.txt','r')
+	genelist=[]
+	startline=1
+	linecount=0
+	for line in genes:
+		linecount+=1
+		data=line.split("\t")
+		if linecount>=startline:
+			genelist.append(data[0].replace("ZFIN:","http://zfin.org/").strip())
+	nodata=[]
+	for geneid in genelist:
+		url="http://pkb-new.nescent.org/kb/gene/eq?id="+geneid.strip()
+		print url
+		content = urllib2.urlopen(url).read()
+		raw = nltk.clean_html(content)
+		data = json.loads(raw)
+		if data:
+			for line in data:
+				if line['entity']:
+					for entity in line['entity']:
+						if line['quality']:
+							for quality in line['quality']:
+								temp=entity.split("http://purl.obolibrary.org/obo/")[1]
+								quality=quality.split("http://purl.obolibrary.org/obo/")[1]
+								print geneid+"\t"+temp+"\t"+quality
+						else:
+							print geneid+"\t"+entity+"\t"+""
+				else:
+					for quality in line['quality']:
+						quality=quality.split("http://purl.obolibrary.org/obo/")[1]
+						print geneid+"\t"+""+"\t"+quality
+		else:
+			nodata.append(geneid)				
+	print "nodata",nodata		
+	
+
+if __name__ == "__main__":
+	import json
+	import urllib2
+	import nltk
+	main()
