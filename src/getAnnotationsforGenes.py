660e382bc62ec8ccb3e976258bb218ac5c0a94a0
prashanti <manda.prashanti@gmail.com>
2016-05-10 11:51:48 -0500

    clean up

--- src/getAnnotationsforGenes.py
+++ src/getAnnotationsforGenes.py
@@ -1,41 +1,47 @@
 def main():
-	genes=open('../data/zfin-genes.txt','r')
+	genes=open('../data/KBGenes_2016.tsv','r')
 	genelist=[]
-	startline=1
+	startline=0
 	linecount=0
+	error=[]
+	genes.next()
 	for line in genes:
-		linecount+=1
-		data=line.split("\t")
-		if linecount>=startline:
-			genelist.append(data[0].replace("ZFIN:","http://zfin.org/").strip())
+		line=line.replace("<","").replace(">","")
+		genelist.append(data[0].strip())
 	nodata=[]
+	print len(genelist)
 	for geneid in genelist:
-		url="http://pkb-new.nescent.org/kb/gene/eq?id="+geneid.strip()
-		content = urllib2.urlopen(url).read()
-		raw = nltk.clean_html(content)
-		data = json.loads(raw)
-		if data:
-			for line in data:
-				if line['entity']:
-					for entity in line['entity']:
-						if line['quality']:
-							for quality in line['quality']:
-								temp=entity.split("http://purl.obolibrary.org/obo/")[1]
-								quality=quality.split("http://purl.obolibrary.org/obo/")[1]
-								print geneid+"\t"+temp+"\t"+quality
-						else:
-							print geneid+"\t"+entity+"\t"+""
-				else:
-					for quality in line['quality']:
-						quality=quality.split("http://purl.obolibrary.org/obo/")[1]
-						print geneid+"\t"+""+"\t"+quality
-		else:
-			nodata.append(geneid)				
-	#print "nodata",nodata		
-	
-
+		try:
+			url="http://kb.phenoscape.org/api/gene/eq?id="+geneid.strip()
+			content = urllib2.urlopen(url).read()
+			data = json.loads(content)
+			if data:
+				for line in data:
+					if line['entity']:
+						for entity in line['entity']:
+							if line['quality']:
+								for quality in line['quality']:
+									temp=entity.split("http://purl.obolibrary.org/obo/")[1]
+									quality=quality.split("http://purl.obolibrary.org/obo/")[1]
+									outfile.write(geneid+"\t"+temp+"\t"+quality+"\n")
+							else:
+								outfile.write(geneid+"\t"+entity+"\t"+""+"\n")
+					else:
+						for quality in line['quality']:
+							quality=quality.split("http://purl.obolibrary.org/obo/")[1]
+							outfile.write(geneid+"\t"+""+"\t"+quality+"\n")
+			else:
+				nodata.append(geneid)	
+		except:
+			error.append(geneid)
+			pass			
+			
+	print "No data", nodata
+	print "Error", error
+	outfile.close()
 if __name__ == "__main__":
 	import json
+	import sys
 	import urllib2
 	import nltk
 	main()
